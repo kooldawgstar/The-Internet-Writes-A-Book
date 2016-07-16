0fea3df005076dcd2604b6629b715f0b67639b66
kooldawgstar <svanikss@gmail.com>
2016-07-16 03:50:04 -0400

    Update index.php
    Added Bitcoin address for donations

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 </div>
 <br>
 <h2>What is this website about?</h2>
-<h4>The goal of this website to collectively write a book with various people on the internet. To contribute to the book all you need to do is read the story and then click the button to type up your sentence and contribute to the book. According to popular writing sites and publishers a book is considered to be 75,000 words, once there is 75,000 words we will put up a notice reminding users we have reached our goal and then 7 days after the notice has been posted the novel will be taken down from the website and published. In the meantime you can work on the next novel that the internet wrote. Please consider turning off ads to help pay for better hosting and the continuation of this website. </h4>
+<h4>The goal of this website to collectively write a book with various people on the internet. To contribute to the book all you need to do is read the story and then click the button to type up your sentence and contribute to the book. According to popular writing sites and publishers a book is considered to be 75,000 words, once there is 75,000 words we will put up a notice reminding users we have reached our goal and then 7 days after the notice has been posted the novel will be taken down from the website and published. In the meantime you can work on the next novel that the internet wrote. Please consider turning off your ad blocker to help pay for better hosting and the continuation of this website. Also currently we accept donations through Bitcoin: 1HqZcEP6hCm8vggTbcZADQmowf8cem3Gyb  </h4>
 <br>
 </html>
 
